cc2e51d38c84bd65a24ecf2779aec022afb2aa6f
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2022-09-09 14:09:16 +0300

    Implement interface, add getPendingIssues, add mock data

--- issue-tracking/src/app/issues.service.ts
+++ issue-tracking/src/app/issues.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Issue } from './issue';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
+  private issues: Issue[] = [];
 
   constructor() { }
+
+  getPendingIssues(): Issue[] {
+    return this.issues.filter(issue =>
+      !issue.completed // displays only issues that are not completed
+    );
+  }
 }

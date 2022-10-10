f7cea20fc8d69f0de4f7596682147b56a40aeeca
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2022-10-10 20:55:50 +0300

    Add click handler

--- star-rating/src/StarRating.jsx
+++ star-rating/src/StarRating.jsx
@@ -2,10 +2,24 @@ import React, { useState } from "react"
 import { FaStar } from "react-icons/fa"
 
 const createArray = length => [...Array(length)]
-
-const Star = ({ selected = false }) => <FaStar color={selected ? "red" : "gray"} />
-
+const Star = ({ selected = false, onSelect = f => f }) => (
+    <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />
+)
 
 export default function StarRating({ totalStars = 5 }) {
-    return createArray(totalStars).map((n, i) => <Star key={i} />)
+    const [selectedStars, setSelectedStars] = useState(0)
+    return (
+        <>
+            {createArray(totalStars).map((n, i) => (
+                <Star 
+                    key={i} 
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectedStars(i + 1)}
+                />
+            ))}
+            <p>
+                Selected {selectedStars} out of {totalStars} stars
+            </p>
+        </>
+    )
 }

0d8ea5d05651ac4b50aa468295bede74ed6f944f
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2022-10-11 19:24:16 +0300

    Add a comment, break Star into its own component

--- star-rating/src/StarRating.jsx
+++ star-rating/src/StarRating.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react"
-import { FaStar } from "react-icons/fa"
+import Star from "./Star"
 
 const createArray = length => [...Array(length)]
-const Star = ({ selected = false, onSelect = f => f }) => (
-    <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />
-)
 
 export default function StarRating({ totalStars = 5 }) {
+    /*
+        The function 'useState' returns an array containing two members.
+        The first member contains the current state and the second member is a function 
+        used to modify the state. Deconstructiing can be used to name these members.
+    */
     const [selectedStars, setSelectedStars] = useState(0)
     return (
         <>

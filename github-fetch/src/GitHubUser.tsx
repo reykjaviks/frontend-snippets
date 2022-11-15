778193262399c598640dbda2d44daa09412d86be
Elizabeth <22527563+reykjaviks@users.noreply.github.com>
2022-11-15 17:52:51 +0200

    Fetch user info from GitHub api

---
+++ github-fetch/src/GitHubUser.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { useState, useEffect } from "react"
+
+// login is the name of the github user's account
+export default function GitHubUser({ login }: { login: string }) {
+  const [data, setData] = useState()
+  const apiBaseURL = "https://api.github.com/users"
+
+  console.log(apiBaseURL.concat("/").concat(login))
+
+  useEffect(() => {
+    if (!login) return
+
+    fetch(apiBaseURL.concat("/").concat(login))
+      .then(response => response.json())
+      .then(setData)
+      .catch(console.error)
+  }, [login])
+
+  if (data)
+    return <pre>{JSON.stringify(data, null, 2)}</pre>
+  return null
+
+}
